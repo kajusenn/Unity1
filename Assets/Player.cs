f04b08e2a34c0b4493c54f12712ae6d05b41d470
kajusen <kajusen88@gmail.com>
2026-06-13 09:11:51 +0200

    % and functions overpower

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -2,78 +2,33 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    //variable
-    //int healthPoints = 10;
-    //string playerName;
-    //SpriteRenderer spriteRenderer;
-
-    //var temp = 23.3f; //ERROR, not in global
-
-    //int hp = 20;
-
-    int points = 0;
-
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        //Debug.Log("I'm Ironman!");
 
-        //variable
-        //playerName = "Adam";
-        //Debug.Log(healthPoints);
-
-        //var temp = 23.3f; //var
-
-        //increment
-        //hp = hp + 5;
-        //hp += 5;
-        //hp -= 2;
-        //hp *= 2;
-        //hp++;
-        //hp--;
-        //--hp;
-        //++hp;
-        //Debug.Log(hp);
-
-        //function
-        //UpdatePoints(points);
-
-        //function with argument
-        //Power(5);
-
-        //function with return
-        //int power = Power(5);
-        //Debug.Log(power);
-
-        //float power = Mathf.Pow(2, 3);
-        //Debug.Log(power);
+        //overpower functions (two functions with the same parameters)
+        //Debug.Log(Power(5)); //take int
+        //Debug.Log(Power(2f)); //take float
 
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Debug.Log((int)Time.time % 4);
 
     }
 
-    //function with return
-    //int Power(int num)
-    //{
-    //    int result = num *= num;
-    //    return result;
-    //}
+    int Power(int num)
+    {
+        int result = num *= num;
+        return result;
+    }
 
-    //function with argument
-    //void Power(int num)
-    //{
-    //    int result = num *= num;
-    //    Debug.Log(result);
-    //}
-
-    //function
-    //void UpdatePoints(int points)
-    //{
-    //    points++;
-    //    Debug.Log(points);
-    //}
+    float Power(float num)
+    {
+        float result = num *= num;
+        return result;
+    }
 }
