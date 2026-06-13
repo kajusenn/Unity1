267c203fcc531e62b8137aa0e257734fb264f508
kajusen <kajusen88@gmail.com>
2026-06-13 09:01:22 +0200

    C# basics 1 variable - functions
    - variable
    - Debug.Log()
    - increment
    - functions basics

---
+++ Assets/Player.cs
@@ -0,0 +1,79 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //variable
+    //int healthPoints = 10;
+    //string playerName;
+    //SpriteRenderer spriteRenderer;
+
+    //var temp = 23.3f; //ERROR, not in global
+
+    //int hp = 20;
+
+    int points = 0;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //Debug.Log("I'm Ironman!");
+
+        //variable
+        //playerName = "Adam";
+        //Debug.Log(healthPoints);
+
+        //var temp = 23.3f; //var
+
+        //increment
+        //hp = hp + 5;
+        //hp += 5;
+        //hp -= 2;
+        //hp *= 2;
+        //hp++;
+        //hp--;
+        //--hp;
+        //++hp;
+        //Debug.Log(hp);
+
+        //function
+        //UpdatePoints(points);
+
+        //function with argument
+        //Power(5);
+
+        //function with return
+        //int power = Power(5);
+        //Debug.Log(power);
+
+        //float power = Mathf.Pow(2, 3);
+        //Debug.Log(power);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    //function with return
+    //int Power(int num)
+    //{
+    //    int result = num *= num;
+    //    return result;
+    //}
+
+    //function with argument
+    //void Power(int num)
+    //{
+    //    int result = num *= num;
+    //    Debug.Log(result);
+    //}
+
+    //function
+    //void UpdatePoints(int points)
+    //{
+    //    points++;
+    //    Debug.Log(points);
+    //}
+}
