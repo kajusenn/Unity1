03b2288ab237872d94a200151f9f32b259972a70
kajusen <kajusen88@gmail.com>
2026-06-24 00:59:54 +0200

    loops and cool tasks, i've done alone

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -3,15 +3,63 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] List<string> cars;
+    //[SerializeField] List<string> cars;
 
     void Start()
     {
-        foreach (string car in cars)
+        //for (int i = 0;i < cars.Count;i++)
+        //{
+        //    Debug.Log(cars[i]);
+        //}
+
+        //foreach (string car in cars)
+        //{
+        //    Debug.Log(car);
+        //}
+
+        //int i = 0;
+        //while (i <= 1000)
+        //{
+        //    Debug.Log(i);
+        //    i++;
+        //}
+
+        //TASK:
+        //for (int i = 10;i <= 20;i++)
+        //{
+        //    if(i % 2 == 0)
+        //    {
+        //        Debug.Log(i);
+        //    }
+        //}
+
+
+        //TASK 2:
+        //string output = "";
+
+        //for (int i = 10;i >= 0;i--)
+        //{
+        //    output += i;
+
+        //    Debug.Log(output += ", ");
+        //}
+
+
+        //TASK 3:
+        List<int> rndNum = new();
+
+        for (int i = 0;i <= 20;i++)
         {
-            Debug.Log(car);
+            int rnd = Random.Range(10, 21);
+            rndNum.Add(rnd);
+
+            //Debug.Log($"{i}: {rndNum[i]}");
+            Debug.Log(rndNum[i]);
+            //Debug.Log(i);
+
         }
-    } 
+    }
+
 
     void Update()
     {
