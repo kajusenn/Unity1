dfa773e3600972ea7cfaf8a620d5e5d69c0a78ae
kajusen <kajusen88@gmail.com>
2026-06-23 13:56:06 +0200

    GetComponent

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,56 +1,23 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Player : MonoBehaviour
 {
-    int hp = 10;
+
+    SpriteRenderer spriteRenderer;
+    [SerializeField] Transform trans;
+    Transform trans2;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.color = Color.blue;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            if(hp != 0)
-            {
-            hp -= 1;
-            Debug.Log("-1HP");
-            Debug.Log($"Health: {hp}");
-            CheckHealth();
-            }
-            else
-            {
-                Debug.Log("YOU ARE DEAD");
-            }
-        }
 
-    }
-
-    void CheckHealth()
-    {
-        if (hp > 6)
-        {
-            //game green
-            Debug.Log("Green");
-        }
-        else if (hp > 2)
-        {
-            //game orange
-            Debug.Log("Orange");
-        }
-        else if (hp >= 1)
-        {
-            //game red
-            Debug.Log("Red");
-        }
-        else
-        {
-            //dead
-            Debug.Log("DEAD");
-        }
     }
 }
