c8b92d701e94614bb45c387eb7b8df561e34fb50
kajusen <kajusen88@gmail.com>
2026-06-23 14:36:32 +0200

    Task getcomponent()

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -3,16 +3,17 @@ using UnityEngine.UIElements;
 
 public class Player : MonoBehaviour
 {
+    //[SerializeField] Transform trans;
+    //Transform trans2;
 
     SpriteRenderer spriteRenderer;
-    [SerializeField] Transform trans;
-    Transform trans2;
+    [SerializeField] Sprite sprite;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
-        spriteRenderer.color = Color.blue;
+        spriteRenderer.sprite = sprite;
     }
 
     // Update is called once per frame
