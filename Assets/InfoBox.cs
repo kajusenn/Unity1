467b94dce91f2a20ab30af8df4781ab054c1e38a
kajusen <kajusen88@gmail.com>
2026-06-26 21:52:39 +0200

    UI

--- Assets/InfoBox.cs
+++ Assets/InfoBox.cs
@@ -2,13 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class InfoBox : MonoBehaviour
 {
     [SerializeField] TMP_Text infoText;
+    [SerializeField] TMP_Text randomNumber;
+    [SerializeField] Button randomBtn;
+
     void Start()
     {
-        Debug.Log(infoText.text);
-        infoText.text = "HELLO MEN!";
+        //Debug.Log(infoText.text);
+        //infoText.text = "HELLO MEN!";
+        //randomBtn.onClick.AddListener(() => RandomNum());
+        randomBtn.onClick.AddListener(delegate {
+            RandomNum(); });
+    }
+
+    private void Update()
+    {
+        infoText.text = Time.time.ToString("F0");
+
+    }
+
+    public void RandomNum()
+    {
+        int rnd = Random.Range(0, 101);
+
+        randomNumber.text = rnd.ToString();
     }
 }
