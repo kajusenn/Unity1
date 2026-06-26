e3d71c5a5318207bb1cd2982a65c5dca472f810b
kajusen <kajusen88@gmail.com>
2026-06-26 09:59:58 +0200

    courutine2

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -5,30 +5,37 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    //IEnumerator myCoroutine;
+    IEnumerator myFade;
+
 
     void Start()
     {
-        //myCoroutine = Die();
+        myFade = Fade();
     }
 
     void Update()
     {
-        //if(Input.GetMouseButtonDown(0))
-        //{
-        //    //StartCoroutine("Die");
-        //    //StartCoroutine(Die());
-        //    StartCoroutine(myCoroutine);
-        //    StopCoroutine(myCoroutine);
-        //}
+        if (Input.GetMouseButtonDown(0))
+        {
+            StartCoroutine(myFade);
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            StopCoroutine(myFade);
+        }
     }
 
-    //IEnumerator Die()
-    //{
-    //    Debug.Log("You Died!");
+    IEnumerator Fade()
+    {
+        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+        //spriteRenderer.color = Color.white; //NOT WORK FROM COURSE
+        Color32 c32 = spriteRenderer.color;
 
-    //    yield return new WaitForSeconds(1f);
-
-    //    Destroy(gameObject);
-    //}
+        for (int i = 255;i >= 0;i--)
+        {
+            yield return new WaitForSeconds(0.02f);
+            spriteRenderer.color = new Color32(c32.r, c32.g, c32.b, (byte)i);
+        }
+    }
 }
